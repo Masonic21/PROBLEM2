9e692a944217bbda9a900b0d4275248e4043e075
Anthony <anthonygolby@hotmail.com>
2016-12-01 09:54:03 -0500

    AnthonyGolby.py

---
+++ AnthonyGolby.py
@@ -0,0 +1,6 @@
+'''Use a substitution format to insert two strings variables into a sentence'''
+
+firstName = "Anthony"
+lastName = "Golby"
+
+print ("Hello my name is %s, my last name is %s" % (firstName, lastName))
